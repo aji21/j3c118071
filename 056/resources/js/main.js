0c47064fc5eb89585236e7b8e4ac595c1fd015fc
aji21 <72715422+aji21@users.noreply.github.com>
2020-10-24 05:58:19 +0700

    Update main.js

--- 056/resources/js/main.js
+++ 056/resources/js/main.js
@@ -7,67 +7,56 @@
         }
        })
 
-    $('#1_nav').fadeIn('1000');
-
     if ($(window).width() < 768) {
         $('#carousel').css("padding-top",$('#navbar').height());
         console.log($('#navbar').height());
     }
 
-
     // scroll functions
     $(window).scroll(function(e) {
-        
         if ($(window).width() > 768)
-        // add/remove class to navbar when scrolling to hide/show
         var scroll = $(window).scrollTop();
         if (scroll >= 50) {
             $('.navbar').addClass("navbar-hide");
         } else {
             $('.navbar').removeClass("navbar-hide");
         }
-    
     });
     
     $("#scrollbtn").click(function() {
         $([document.documentElement, document.body]).animate({
-            scrollTop: $("#3_nations").offset().top
+            scrollTop: $("#nations3").offset().top
         }, 2000);
-        $([document.documentElement, document.body]).delay("3000").animate({
-            scrollTop: $("#4_specials").offset().top
+        $([document.documentElement, document.body]).delay("2500").animate({
+            scrollTop: $("#3specials4").offset().top
         }, 2000);
-        $([document.documentElement, document.body]).delay("3000").animate({
-            scrollTop: $("#5_company").offset().top
+        $([document.documentElement, document.body]).delay("2500").animate({
+            scrollTop: $("#company5").offset().top
         }, 2000);
-        $([document.documentElement, document.body]).delay("3000").animate({
-            scrollTop: $("#6_work").offset().top
+        $([document.documentElement, document.body]).delay("2500").animate({
+            scrollTop: $("#work6").offset().top
         }, 2000);
-        $([document.documentElement, document.body]).delay("3000").animate({
-            scrollTop: $("#7_recruit").offset().top
+        $([document.documentElement, document.body]).delay("2500").animate({
+            scrollTop: $("#recruit7").offset().top
         }, 2000);
-        $([document.documentElement, document.body]).delay("3000").animate({
-            scrollTop: $("#8_footer").offset().top
+        $([document.documentElement, document.body]).delay("2500").animate({
+            scrollTop: $("#footer9").offset().top
         }, 2000);
-
-        
     });
     
     $("#gotop").click(function() {
         $([document.documentElement, document.body]).animate({
-            scrollTop: $("#1_nav").offset().top
+            scrollTop: $("#nav1").offset().top
         }, 2000);
     });
 
     $(window).scroll(function() {
         var windowBottom = $(this).scrollTop() + $(this).innerHeight();
         $(".fade").each(function() {
-          /* Check the location of each desired element */
           var objectTop = $(this).offset().top ;
-          
-          /* If the element is completely within bounds of the window, fade it in */
-          if (objectTop < windowBottom) { //object comes into view (scrolling down)
+          if (objectTop < windowBottom) {
             if ($(this).css("opacity")==0) {$(this).delay(1000).fadeTo(500,1);}
           }
         });
-      }).scroll(); //invoke scroll-handler on page-load
+      }).scroll();
 })(jQuery);    
